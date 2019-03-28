d2a106c3973b48b5d1964b53924ad5e17c6d5124
Benjamin Carman <bc818216@ohio.edu>
2019-03-28 06:20:25 -0400

    Thin lines before finding corners

--- find-corners/find-corners.cc
+++ find-corners/find-corners.cc
@@ -13,6 +13,8 @@ struct Config {
   Mat hsv_img_thresholded;
   Mat noise_free;
   Mat boundary;
+  Mat thinned_boundary;
+  Mat boundary_corners;
   Mat corners;
   Mat final;
 
@@ -43,6 +45,13 @@ bool isEdge(Mat &image, int i, int j)
   return false;
 }
 
+/*
+could fit a quadraitc to area of 3 or 5 points. coefficiencts yield second partial derivatives for curvature
+
+could use 4, 6, or 8 simple to thin the line and get rid of pixels then compute again
+
+*/
+
 void drawBoundary(Mat &source, Mat &target, int i, int j, std::vector<Point> &boundary_points)
 {
   Point p;
@@ -61,60 +70,224 @@ void drawBoundary(Mat &source, Mat &target, int i, int j, std::vector<Point> &bo
   if (isEdge(source, i - 1, j - 1) && target.at<unsigned char>(i - 1, j - 1) == 0) drawBoundary(source, target, i - 1, j - 1, boundary_points);
 }
 
+bool changesConnectivity(Mat &image, int i, int j)
+{
+  int num_neighbors = 0;
+  Mat local_area = Mat::zeros(3,3, CV_8U);
+
+  for (int n = 0; n < 3; n++)
+  {
+    for (int m = 0; m < 3; m++)
+    {
+      //Keep the center a 0
+      if (!(n == 1 && m == 1))
+      {
+        local_area.at<unsigned char>(n,m) = image.at<unsigned char>(i - 1 + n, j - 1 + m);
+
+        if (local_area.at<unsigned char>(n,m) == 255)
+        {
+          num_neighbors++;
+        }
+      }
+    }
+  }
+  /*
+  if (local_area.at<unsigned char>(0,0) == 0 && local_area.at<unsigned char>(0,1) == 0 &&
+      local_area.at<unsigned char>(0,2) == 0 && local_area.at<unsigned char>(1,0) == 0 &&
+      local_area.at<unsigned char>(1,2) == 0 && local_area.at<unsigned char>(2,0) == 255 &&
+      local_area.at<unsigned char>(2,1) == 255 && local_area.at<unsigned char>(2,2) == 255){
+        std::cout << local_area << std::endl;
+      }
+  */
+  if (num_neighbors > 1)
+  {
+    Mat labels;
+    int num_components = connectedComponents(local_area, labels, 8);
+    if (num_components == 2) //Only a foreground and background
+    {
+      return false;
+    }
+  }
+  return true;
+/*
+  if (image.at<unsigned char>(i,j - 1) == 255 && image.at<unsigned char>(i,j + 1) == 255 &&
+      image.at<unsigned char>(i - 1,j) == 0   && image.at<unsigned char>(i + 1,) == 0)
+  {
+    changes_connectivity = true;
+  }
+
+  if (image.at<unsigned char>(i,j - 1) == 255 && image.at<unsigned char>(i,j + 1) == 255 &&
+      image.at<unsigned char>(i - 1,j) == 0   && image.at<unsigned char>(i + 1,) == 0)
+  {
+    changes_connectivity = true;
+  }
+*/
+}
+
+void thin(Mat &image)
+{
+  bool was_changed = true;
+  int count = 0;
+  while (was_changed)
+  {
+    was_changed = false;
+
+    //Thin from top
+    for (int j = 0; j < image.cols; j++)
+    {
+      for (int i = 0; i < image.rows; i++)
+      {
+        if (image.at<unsigned char>(i,j) == 255)
+        {
+          count++;
+          if (!changesConnectivity(image, i, j))
+          {
+            image.at<unsigned char>(i,j) = 0;
+            was_changed = true;
+          }
+          break;
+        }
+      }
+    }
+    std::cout << count;
+
+    //Thin from right
+    for (int i = 0; i < image.rows; i++)
+    {
+      for (int j = image.cols - 1; j >= 0; j--)
+      {
+        if (image.at<unsigned char>(i,j) == 255)
+        {
+          if (!changesConnectivity(image, i, j))
+          {
+            image.at<unsigned char>(i,j) = 0;
+            was_changed = true;
+          }
+          break;
+        }
+      }
+    }
+
+    //Thin from bottom
+    for (int j = 0; j < image.cols; j++)
+    {
+      for (int i = image.rows - 1; i >= 0; i--)
+      {
+        if (image.at<unsigned char>(i,j) == 255)
+        {
+          if (!changesConnectivity(image, i, j))
+          {
+            image.at<unsigned char>(i,j) = 0;
+            was_changed = true;
+          }
+          break;
+        }
+      }
+    }
+
+    //Thin from left
+    for (int i = 0; i < image.rows; i++)
+    {
+      for (int j = 0; j < image.cols; j++)
+      {
+        if (image.at<unsigned char>(i,j) == 255)
+        {
+          if (!changesConnectivity(image, i, j))
+          {
+            image.at<unsigned char>(i,j) = 0;
+            was_changed = true;
+          }
+          break;
+        }
+      }
+    }
+  }
+}
+
+
+
+
 void get_corners(std::vector<Point> boundary_points, std::vector<Point> &corner_points, Mat &image)
 {
 
   std::vector<double> slopes;
-
+  int buffer = 3;
+  //Get all out slopes from orgigin +- 3 pixels
   for (size_t i = 0; i < boundary_points.size(); i++)
   {
-    if (i < 5)
+    if (i < buffer)
     {
-      std::vector<Point> pts (boundary_points.begin(), boundary_points.begin() + (i + 5));
+      std::vector<Point> pts (boundary_points.begin(), boundary_points.begin() + (i + buffer));
 
-      for (int j = boundary_points.size() - (5 - i); j < boundary_points.size(); j++)
+      for (int j = boundary_points.size() - (buffer - i); j < boundary_points.size(); j++)
       {
         pts.push_back(boundary_points[j]);
       }
-
+      std::cout<< "#pts: " << pts.size()<< std::endl;
       Vec4f line; //use l2
-      fitLine(pts, line, cv::DIST_FAIR, 1, 0.001, 0.001);
-
+      fitLine(pts, line, cv::DIST_L2, 1, 0.01, 0.01);
+      /*
+      Point pt1, pt2;
+      pt1.x = line[2] - 100*line[0];
+      pt1.y = line[3] - 100*line[1];
+      pt2.x = line[2] + 100*line[0];
+      pt2.y = line[3] + 100*line[1];
+      cv::line(image, pt1, pt2, Scalar(255,0,0), 1);
+      */
       double slope = line[1]/line[0];
       slopes.push_back(slope);
       //std::cout << line[0] << " " << line[1] << " " << line[2] << " " << line[3] << " " << slope << std::endl;
     }
-    else if (i >= boundary_points.size() - 5)
+    else if (i >= boundary_points.size() - buffer)
     {
-      std::vector<Point> pts (boundary_points.begin() + (i - 5), boundary_points.end());
+      std::vector<Point> pts (boundary_points.begin() + (i - buffer), boundary_points.end());
 
-      for (int j = 0; j < 6 - (boundary_points.size() - i); j++)
+      for (int j = 0; j < (buffer + 1) - (boundary_points.size() - i); j++)
       {
         pts.push_back(boundary_points[j]);
       }
+      std::cout<< "#pts: " << pts.size()<< std::endl;
 
       Vec4f line;
-      fitLine(pts, line, cv::DIST_FAIR, 1, 0.001, 0.001);
-
+      fitLine(pts, line, cv::DIST_L2, 1, 0.01, 0.01);
+      /*
+      Point pt1, pt2;
+      pt1.x = line[2] - 100*line[0];
+      pt1.y = line[3] - 100*line[1];
+      pt2.x = line[2] + 100*line[0];
+      pt2.y = line[3] + 100*line[1];
+      cv::line(image, pt1, pt2, Scalar(255,0,0), 1);
+      */
       double slope = line[1]/line[0];
       slopes.push_back(slope);
       //std::cout << line[0] << " " << line[1] << " " << line[2] << " " << line[3] << " " << slope << std::endl;
     }
     else
     {
-    std::vector<Point> pts (boundary_points.begin() + (i - 5), boundary_points.begin() + (i + 5));
+    std::vector<Point> pts (boundary_points.begin() + (i - buffer), boundary_points.begin() + (i + buffer));
+    std::cout<< "#pts: " << pts.size()<< std::endl;
 
     Vec4f line;
-    fitLine(pts, line, cv::DIST_FAIR, 1, 0.001, 0.001);
-
+    fitLine(pts, line, cv::DIST_L2, 1, 0.01, 0.01);
+    /*
+    Point pt1, pt2;
+    pt1.x = line[2] - 100*line[0];
+    pt1.y = line[3] - 100*line[1];
+    pt2.x = line[2] + 100*line[0];
+    pt2.y = line[3] + 100*line[1];
+    cv::line(image, pt1, pt2, Scalar(255,0,0), 1);
+    */
     double slope = line[1]/line[0];
     slopes.push_back(slope);
-    std::cout << line[0] << " " << line[1] << " " << line[2] << " " << line[3] << " " << slope << std::endl;
+    //std::cout << line[0] << " " << line[1] << " " << line[2] << " " << line[3] << " " << slope << std::endl;
     }
     //std::cout << boundary_points[i] << std::endl;
   }
 
+  //Compute slope changes (gradient)
   std::vector<double> slope_change;
+  //Change over t=2
+  /*
   slope_change.push_back((slopes[1] - slopes[slopes.size() - 1])/2);
   for (int i = 1; i < slopes.size() - 1; i++)
   {
@@ -123,6 +296,15 @@ void get_corners(std::vector<Point> boundary_points, std::vector<Point> &corner_
     std::cout << change << std::endl;
   }
   slope_change.push_back((slopes[0] - slopes[slopes.size() - 2])/2);
+  */
+  //Change over t=1
+  for (int i = 0; i < slopes.size() - 1; i++)
+  {
+    double change = (slopes[i+1] - slopes[i]);
+    slope_change.push_back(change);
+    //std::cout << change << std::endl;
+  }
+  slope_change.push_back(slopes[0] - slopes[slopes.size() - 1]);
 
   int locs[8];
   for (int l = 0; l < 8; l++)
@@ -131,13 +313,18 @@ void get_corners(std::vector<Point> boundary_points, std::vector<Point> &corner_
     int loc;
     for (size_t i = 0; i < slope_change.size(); i++)
     {
+      std::cout << "Iteration: " << l << ", i: " << i << ", Slope Change: " << slope_change[i] <<
+                   ", Slope[i]: " << slopes[i] << ", Slope[i+1]: " << slopes[i+1] << std::endl;
       if (abs(slope_change[i]) > max)
       {
-        max = slope_change[i];
+        max = abs(slope_change[i]);
         loc = i;
       }
     }
 
+    std::cout << "Location " << loc << "was chosen" << std::endl;
+    slope_change[loc] = 0;
+    /*
     int start = (slope_change.size() + (loc - (slope_change.size()/10))) % slope_change.size();
     std::cout << "Loc: " << loc << " Start: " << start << std::endl;
     int count = 2 * (slope_change.size()/10);
@@ -154,13 +341,47 @@ void get_corners(std::vector<Point> boundary_points, std::vector<Point> &corner_
         start++;
       }
     }
+    *////////
     /*
     for (int i = (slope_change.size() + (loc - (slope_change.size()/12))) % slope_change.size(); i < (loc + (slope_change.size()/12)) % slope_change.size(); i++)
     {
       slope_change[loc] = 0;
       std::cout << "Dude";
     }
-*/
+    */
+    int erasable_range = slope_change.size()/9; //10
+    if (loc < erasable_range)
+    {
+      for (int i = 0; i <= loc + erasable_range; i++)
+      {
+        slope_change[i] = 0;
+      }
+
+      for (int i = slope_change.size() - (erasable_range - loc); i < slope_change.size(); i++)
+      {
+        slope_change[i] = 0;
+      }
+    }
+    else if (loc > slope_change.size() - (erasable_range + 1))
+    {
+      for (int i = loc - erasable_range; i <= slope_change.size(); i++)
+      {
+        slope_change[i] = 0;
+      }
+
+      for (int i = 0; i <= erasable_range - (slope_change.size() - loc); i++)
+      {
+        slope_change[i] = 0;
+      }
+    }
+    else
+    {
+      for (int i = loc - erasable_range; i <= loc + erasable_range; i++)
+      {
+        slope_change[i] = 0;
+      }
+    }
+
     locs[l] = loc;
   }
 
@@ -171,6 +392,8 @@ void get_corners(std::vector<Point> boundary_points, std::vector<Point> &corner_
 
     circle(image, boundary_points[locs[i]], 5, Scalar(255, 0, 0));
   }
+
+
 
 /*
   std::vector<Point> slope_time;
@@ -224,7 +447,9 @@ int main(int argc, char** argv)
     namedWindow("HSV Image Thresholded", WINDOW_AUTOSIZE);
     namedWindow("Noise Free Image", WINDOW_AUTOSIZE);
     namedWindow("Boundary", WINDOW_AUTOSIZE);
-    namedWindow("Final Image", WINDOW_AUTOSIZE);
+    namedWindow("Thinned Boundary", WINDOW_AUTOSIZE);
+    namedWindow("Corner Points", WINDOW_AUTOSIZE);
+    //namedWindow("Final Image", WINDOW_AUTOSIZE);
     //namedWindow("Threshold Slider", WINDOW_AUTOSIZE);
 
     r.low_h = 0;
@@ -271,14 +496,39 @@ int main(int argc, char** argv)
     }
 
     imshow("Noise Free Image", r.noise_free);
-
-    //Find corners
-    std::vector<Point> corner_points;
-    get_corners(boundary_points, corner_points, r.boundary);
-
     imshow("Boundary", r.boundary);
 
+    r.thinned_boundary = r.boundary;
+    thin(r.thinned_boundary);
+    imshow("Thinned Boundary", r.thinned_boundary);
 
+
+    Mat dummy = Mat::zeros(r.thinned_boundary.rows, r.thinned_boundary.cols, CV_8U);
+    std::vector<Point> thinned_boundary_points;
+
+    //Find thinned boundary points
+    done = false;
+    for(int i = 0; i < r.thinned_boundary.rows; i++) {
+      if (done) break;
+      for(int j = 0; j < r.thinned_boundary.cols; j++) {
+        if (isEdge(r.thinned_boundary, i, j))
+        {
+          drawBoundary(r.thinned_boundary, dummy, i, j, thinned_boundary_points);
+          done = true;
+          break;
+        }
+      }
+    }
+
+
+    r.boundary_corners = r.thinned_boundary;
+    //Find corners
+    std::vector<Point> corner_points;
+    get_corners(thinned_boundary_points, corner_points, r.boundary_corners);
+
+    imshow("Corner Points", r.boundary_corners);
+
+/*
     r.line_length = 25;
     //createTrackbar("Minimum Line Length Selector", "Threshold Slider", &r.line_length, 100, slider, &r);
     //slider(0, &r);
@@ -286,7 +536,7 @@ int main(int argc, char** argv)
 
     std::vector<Vec2f> lines;
     std::vector<Vec2f> unique_lines;
-    HoughLines(r.boundary, lines, 1, 3*CV_PI/180, r.line_length);
+    HoughLines(r.thinned_boundary, lines, 1, 3*CV_PI/180, r.line_length);
 
 
     int num_unique = 0;
@@ -302,7 +552,7 @@ int main(int argc, char** argv)
     }
 
 
-    r.final = r.boundary.clone();
+    r.final = r.thinned_boundary.clone();
     for (size_t i = 0; i < num_unique; i++)
       {
         float rho = unique_lines[i][0], theta = unique_lines[i][1];
@@ -318,13 +568,16 @@ int main(int argc, char** argv)
       }
 
     imshow("Final Image", r.final);
-
+*/
   	waitKey(0);
     destroyWindow("Original Image");
     destroyWindow("HSV Image Thresholded");
     destroyWindow("Noise Free Image");
     destroyWindow("Boundary");
-    destroyWindow("Final Image");
+    destroyWindow("Thinned Boundary");
+    destroyWindow("Corner Points");
+
+    //destroyWindow("Final Image");
     //destroyWindow("Threshold Slider");
 
   }
